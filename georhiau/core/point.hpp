bc4d39832ffeb0d818757285dc7eff2123ba33af
llyr-who <jamesbushnell1992@gmail.com>
2020-02-25 18:47:24 +0000

    begin testing

--- georhiau/core/point.hpp
+++ georhiau/core/point.hpp
@@ -9,11 +9,16 @@ namespace core {
 template <typename T, std::size_t D>
 class point {
 public:
-    point() {
+    point<typename... Ts,
+          typename std::enable_if<
+              std::conjunction<std::is_same<T, Ts>...>::value &&
+                  (sizeof...(Ts) == R * C),
+              int>::type = 0> constexpr matrix(Ts&&... elements) noexcept {
+        m_data = std::array<T, R * C>{std::forward<Ts>(elements)...};
         //! We could end up only having D = 2,3.
         //! As these are the most commonly used dims.
         static_assert(D == 2, "Only implemented for D == 2.");
-    };
+    }
 
     bool operator<(const point<T, D>& r) {
         return m_data[0] < r.m_data[0] ||
---
+++ georhiau/core/tests/point_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "georhiau/core/point.hpp"
+
+using point = georhiau::core::point<double,2>;
+
+TEST(point, less_than) {
+    
+}
