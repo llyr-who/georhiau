814a118b0d9037914037cc52aa34ac6dcdcae5a8
llyr-who <jamesbushnell1992@gmail.com>
2020-03-17 10:21:21 +0000

    triangulation of square

--- georhiau/algo/tests/delaunay_test.cpp
+++ georhiau/algo/tests/delaunay_test.cpp
@@ -20,3 +20,24 @@ TEST(delaunay, single_element_mesh) {
     ASSERT_EQ(tngltn.front(), t);
 }
 
+TEST(delaunay, square) {
+    const vertex a = {0.0, 0.0};
+    const vertex b = {2.0, 0.0};
+    const vertex c = {2.0, 2.0};
+    const vertex d = {0.0, 2.0};
+
+    std::vector<vertex> cloud = {a, b, c, d};
+
+    auto tngltn = georhiau::algo::delaunay(cloud);
+
+    georhiau::core::triangle<double> t1 = {a, b, d};
+    georhiau::core::triangle<double> t2 = {d, b, c};
+
+    auto it1 = std::find(tngltn.begin(), tngltn.end(), t1);
+    auto it2 = std::find(tngltn.begin(), tngltn.end(), t2);
+
+    ASSERT_FALSE(it1 == tngltn.end());
+    ASSERT_FALSE(it2 == tngltn.end());
+
+    ASSERT_EQ(tngltn.size(), 2);
+}
