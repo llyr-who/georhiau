149fd38e820d9f9347fe4b757e0cd932f92644bb
llyr-who <jamesbushnell1992@gmail.com>
2020-03-18 16:57:17 +0000

    circle example failing

---
+++ georhiau/examples/circle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+#include "algo/delaunay.hpp"
+#include "core/vertex.hpp"
+
+using vertex = georhiau::core::vertex<double, 2>;
+
+int main() {
+    std::vector<vertex> cloud;
+
+    std::size_t N = 10;
+    for (std::size_t i = 0; i < N; ++i) {
+        double t = 2 * 3.1459 * (i / N);
+        cloud.push_back(vertex{std::cos(t), std::sin(t)});
+    }
+    cloud.push_back(vertex{0.0, 0.0});
+
+    std::cout << "input cloud" << std::endl;
+    std::cout << cloud.size() << std::endl;
+    auto tris = georhiau::algo::delaunay(cloud);
+
+    std::cout << "Number of triangles" << std::endl;
+    std::cout << tris.size() << std::endl;
+    for (const auto& tri : tris) {
+        georhiau::core::print(tri);
+    }
+}
