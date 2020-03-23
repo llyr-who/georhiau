a6f34db2f76fccd6b24e09537545981b30606f87
llyr-who <jamesbushnell1992@gmail.com>
2020-03-23 19:34:05 +0000

    increase robustness of frontier

--- georhiau/algo/delaunay.hpp
+++ georhiau/algo/delaunay.hpp
@@ -22,55 +22,79 @@ template <typename T>
 using triangle = georhiau::core::triangle<T>;
 
 template <typename T>
-void print_frontier(const std::set<edge<T>>& frontier) {
-    std::cout << "state of front" << std::endl;
-    for (const auto& e : frontier) {
-        georhiau::core::print(e);
+class frontier {
+private:
+    // private helper class to encap. an edge
+    struct f_item {
+        edge<T> m_e;
+
+        f_item(const vertex<T>& a, const vertex<T>& b) : m_e{a, b} {}
+        f_item(const edge<T>& e) : m_e{e} {}
+        // loosing equality on edge
+        bool operator==(const f_item& fi) {
+            auto e_rot = georhiau::core::rotate(fi.m_e);
+            if (m_e == fi.m_e || m_e == e_rot) return true;
+            return false;
+        }    
+    };
+
+    std::set<edge<T>> m_fron;
+    std::vector<f_item> m_dead;
+
+public:
+    frontier() {}
+
+    void insert(const edge<T>& e) {
+        m_fron.insert(e);
     }
-}
+    
+    void update(const vertex<T>& a, const vertex<T>& b) {
+        auto e = edge<T>{a, b};
+        auto it_fron = m_fron.find(e);
+        if (it_fron == m_fron.end()) {
+            auto fi = f_item(e);
+            auto it_dead = std::find(m_dead.begin(), m_dead.end(), fi);
+            if (it_dead == m_dead.end()) {
+                edge<T> re{b, a};
+                m_fron.insert(re);
+            }
+        } else {
+            m_fron.erase(it_fron);
+            m_dead.push_back(*it_fron);
+        }
+    }
+
+    auto pop_min() {
+        auto min = *m_fron.begin();
+        m_fron.erase(m_fron.begin());
+        return min;
+    }
+
+    bool empty() const { return m_fron.empty(); }
+
+    auto dump() const { return m_fron; }
+};
 
 template <typename T>
-void update_frontier(std::set<edge<T>>& frontier, const vertex<T>& a,
-                     const vertex<T>& b) {
-    edge<T> e(a, b);
-    // std::cout << "edge we are looking for" << std::endl;
-    // georhiau::core::print(e);
-    std::cout << " we are looking for " << std::endl;
-    georhiau::core::print(e);
-    std::cout << "what is in there" << std::endl;
-    print_frontier(frontier);
-    auto it = frontier.find(e);
-    if (it == frontier.end()) {
-        auto flip = georhiau::core::flip(e);
-        frontier.insert(flip);
-    } else {
-        //    std::cout << "got it" << std::endl;
-        frontier.erase(it);
+void print(const frontier<T>& f) {
+    for (const auto& e : f.dump()) {
+        georhiau::core::print(e);
     }
 }
 
 template <typename T>
 auto delaunay(std::vector<vertex<T>>& cloud) {
     std::list<triangle<T>> triangles;
-    std::set<edge<T>> frontier;
+    frontier<T> f;
     auto e = georhiau::algo::first_hull_edge<T>(cloud);
-    frontier.insert(e);
-    std::size_t I = 0;
-    while (!frontier.empty()) {
-        auto e = *frontier.begin();
-        //        print_frontier(frontier);
-        frontier.erase(frontier.begin());  // remove min.
+    f.insert(e);
+    while (!f.empty()) {
+        auto e = f.pop_min();
         auto p = mate2(e, cloud);
         if (p == cloud.end()) continue;
-        update_frontier(frontier, *p, e.orig());
-        update_frontier(frontier, e.dest(), *p);
+        f.update(*p, e.orig());
+        f.update(e.dest(), *p);
         triangles.push_back(triangle<T>{e.orig_ref(), e.dest_ref(), *p});
-        auto t = triangle<T>{e.orig_ref(), e.dest_ref(), *p};
-        /*
-        georhiau::core::print(t);
-        std::cout << std::endl;
-        std::cout << I++ << std::endl;
-         */
     }
     return triangles;
 }
