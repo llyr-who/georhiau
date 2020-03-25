bef4d0fc6a6c2462e12e0e3462721de040eda364
llyr-who <jamesbushnell1992@gmail.com>
2020-03-25 14:17:36 +0000

    tidy up

--- georhiau/algo/delaunay.hpp
+++ georhiau/algo/delaunay.hpp
@@ -25,7 +25,6 @@ template <typename T>
 class frontier {
 private:
     std::set<edge<T>> m_fron;
-    std::set<edge<T>> m_dead;
 
 public:
     frontier() {}
@@ -35,26 +34,6 @@ public:
     bool update(const vertex<T>& a, const vertex<T>& b) {
         auto e = edge<T>{a, b};
         auto e_rev = edge<T>{b, a};
-
-        // If we find the reverse then we need to quit early...
-        auto it_rev_fron = m_fron.find(e_rev);
-        // as we have not yet popped it off and processed it.
-        // If we continue, we are going to unnecc. add it (duplicate entries).
-        if (it_rev_fron != m_fron.end()) {
-            return false;
-        }
-
-        // If we have processed the edge then we need to check the graveyard.
-        // If it has been processed then it is in the graveyard.
-        // Remember an edge is considered dead if both it (e) and
-        // its reverse (e_rev) are in the graveyard.
-        // Since we place both in the graveyard we only need to search for
-        // the edge XOR its reverse.
-        auto it_rev_dead = m_dead.find(e_rev);
-        if (it_rev_dead != m_dead.end()) {
-            return false;
-        }
-
         // Now we look for the edge.
         auto it_fron = m_fron.find(e);
         if (it_fron == m_fron.end()) {
@@ -66,10 +45,7 @@ public:
             return true;
         } else {
             // If we found the edge then we need to remove it from the
-            // frontier as it has already been dealt with and we should
-            // NOT add a triangle that has e as a face.
-            m_dead.insert(e);
-            m_dead.insert(e_rev);
+            // frontier as it has already been dealt with.
             m_fron.erase(it_fron);
             return false;
         }
@@ -77,9 +53,6 @@ public:
 
     auto pop_min() {
         auto min = *m_fron.begin();
-        auto min_rot = edge<T>(min.dest(), min.orig());
-        m_dead.insert(min);
-        m_dead.insert(min_rot);
         m_fron.erase(m_fron.begin());
         return min;
     }
@@ -87,12 +60,7 @@ public:
     bool empty() const { return m_fron.empty(); }
 
     void print() const {
-        std::cout << "m_fron" << std::endl;
         for (const auto& e : m_fron) {
-            georhiau::core::print(e);
-        }
-        std::cout << "m_dead" << std::endl;
-        for (const auto& e : m_dead) {
             georhiau::core::print(e);
         }
     }
