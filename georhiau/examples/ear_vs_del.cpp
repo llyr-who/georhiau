8ea91cfe11f4577796fcaffd3e329ddbe730b67b
llyr-who <jamesbushnell1992@gmail.com>
2020-04-02 11:24:35 +0100

    comparison complete

--- georhiau/examples/ear_vs_del.cpp
+++ georhiau/examples/ear_vs_del.cpp
@@ -5,16 +5,25 @@
 int main() {
     using vertex = georhiau::core::vertex<double, 2>;
 
-    vertex a = {1.0, 5.0};
-    vertex b = {1.0, 1.0};
-    vertex c = {3.0, 1.0};
-    vertex d = {3.0, 3.0};
+    // polygon 1
+    vertex a = {0.1, 2.1};
+    vertex b = {0.1, 0.1};
+    vertex c = {2.0, 0.1};
+    vertex d = {2.0, 2.0};
+    std::vector<vertex> cloud1 = {a, b, c, d};
 
-    std::vector<vertex> cloud = {a,b,c,d};
+    // polygon 2
+    a = {0.1, 1.9};
+    std::vector<vertex> cloud2 = {a, b, c, d};
 
-    //auto tris = georhiau::algo::delaunay(cloud);
-    //georhiau::view::plot(tris);
-    auto tris = georhiau::algo::ear_clip(cloud);
+    auto tris = georhiau::algo::delaunay(cloud1);
+    georhiau::view::plot(tris);
+    tris = georhiau::algo::ear_clip(cloud1);
+    georhiau::view::plot(tris);
+
+    tris = georhiau::algo::delaunay(cloud2);
+    georhiau::view::plot(tris);
+    tris = georhiau::algo::ear_clip(cloud2);
     georhiau::view::plot(tris);
 }
 
