f55b9e254096fb30967a28921a170762ee46825b
llyr-who <jamesbushnell1992@gmail.com>
2020-04-09 10:44:51 +0100

    embelish test

--- georhiau/algo/tests/rng_test.cpp
+++ georhiau/algo/tests/rng_test.cpp
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 
 using vertex = georhiau::core::vertex<double, 2>;
+using edge = georhiau::core::edge<double, 2>;
 
 TEST(rng, basic) {
     vertex a = {0.0, 0.0};
@@ -13,6 +14,14 @@ TEST(rng, basic) {
     std::vector<vertex> cloud = {a, b, c};
 
     auto rng = georhiau::algo::rng(cloud);
+
     ASSERT_EQ(rng.size(), 4);
+
+    edge e1(a, b), e2(b, a), e3(b, c), e4(c, b);
+
+    ASSERT_TRUE(std::find(rng.begin(), rng.end(), e1) != rng.end());
+    ASSERT_TRUE(std::find(rng.begin(), rng.end(), e2) != rng.end());
+    ASSERT_TRUE(std::find(rng.begin(), rng.end(), e3) != rng.end());
+    ASSERT_TRUE(std::find(rng.begin(), rng.end(), e4) != rng.end());
 }
 
